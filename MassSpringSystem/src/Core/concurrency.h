b753790a9308f4121fd58c338c880e70332d2077
nat mat <natmat101@hotmail.com>
2022-01-31 18:47:47 +0000

    Commited test code for non work stealing thread pool

--- MassSpringSystem/src/Core/concurrency.h
+++ MassSpringSystem/src/Core/concurrency.h
@@ -212,6 +212,8 @@ public:
 	/*template<typename FunctionType>
 	auto submit(FunctionType f)->std::future<typename std::result_of<FunctionType()>>*/
 
+	//refer to this blog to understand this approach https://vorbrodt.blog/2019/02/09/template-concepts-sort-of/
+	//The book didnt consider cases of multiple args
 	//attempt at perfect forwarding
 	template<typename FunctionType, typename... Args>
 	std::future<typename std::invoke_result_t<FunctionType, Args...>> submit(FunctionType&& f, Args&&... args)
@@ -227,46 +229,47 @@ public:
 };
 
 
-//https://github.com/Toeplitz/threadpool example has been taken from here
-int main()
-{
-
-	thread_pool tp;
-	uint64_t N = 100;
-
-	std::atomic_int ongoing_count(0);
-	std::atomic_int completed_count(0);
-
-	for (uint64_t n(0); n < N; ++n)
-	{
-		// Delay submitting to get clean looking output...
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-		tp.submit([n, &ongoing_count, &completed_count]() {
-			ongoing_count++;
-
-			const uint32_t sleep_time = 1000;
-			std::thread::id this_id = std::this_thread::get_id();
-			std::cout << "[ " << n << "] sleeping for " << sleep_time << " in thread " << this_id << std::endl;
-			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
-			std::cout << "[ " << n << "] exiting ..." << std::endl;
-
-			completed_count++;
-			ongoing_count--;
-		});
-	}
-
-	while (1)
-	{
-		std::cout << "ongoing: " << ongoing_count << ", completed: " << completed_count
-			<< " / " << N << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-		if (completed_count == N)
-		{
-			std::cout << "Finished: " << completed_count << std::endl;
-			break;
-		}
-	}
-
-}
+////https://github.com/Toeplitz/threadpool example has been taken from here
+//int main()
+//{
+//
+//	thread_pool tp;
+//	uint64_t N = 100;
+//
+//	std::atomic_int ongoing_count(0);
+//	std::atomic_int completed_count(0);
+//
+//	for (uint64_t n(0); n < N; ++n)
+//	{
+//		// Delay submitting to get clean looking output...
+//		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+//
+//		tp.submit([n, &ongoing_count, &completed_count]() {
+//			ongoing_count++;
+//
+//			const uint32_t sleep_time = 1000;
+//			std::thread::id this_id = std::this_thread::get_id();
+//			std::cout << "[ " << n << "] sleeping for " << sleep_time << " in thread " << this_id << std::endl;
+//			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+//			std::cout << "[ " << n << "] exiting ..." << std::endl;
+//
+//			completed_count++;
+//			ongoing_count--;
+//		});
+//	}
+//
+//	while (1)
+//	{
+//		std::cout << "ongoing: " << ongoing_count << ", completed: " << completed_count
+//			<< " / " << N << std::endl;
+//		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//
+//		if (completed_count == N)
+//		{
+//			std::cout << "Finished: " << completed_count << std::endl;
+//			break;
+//		}
+//	}
+//
+//}
+//
